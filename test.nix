4adcf10e3112c9e94e569acf49997a05812b00de
Ali Jamadi <jamadi1377@gmail.com>
2025-03-01 02:23:40 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:test.nix

--- test.nix
+++ test.nix
@@ -10,17 +10,7 @@ let
         # non-hermetic default.
         system = null;
 
-        modules = args.modules ++ [
-          # This module is injected here since it exposes the nixpkgs self-path in as
-          # constrained of contexts as possible to avoid more things depending on it and
-          # introducing unnecessary potential fragility to changes in flakes itself.
-          #
-          # See: failed attempt to make pkgs.path not copy when using flakes:
-          # https://github.com/NixOS/nixpkgs/pull/153594#issuecomment-1023287913
-          ({ config, pkgs, lib, ... }: {
-            config.nixpkgs.flake.source = self.outPath;
-          })
-        ];
+        modules = args.modules;
       } // builtins.removeAttrs args [ "modules" ]
     );
 
@@ -42,13 +32,13 @@ let
       else if lib.isList val then
         map toJSONLossy val
       else
-        builtins.toJSON val;
+        builtins.toJSON (lib.trace_val val);
 
-  nixos = (lib.traceVal nixosLib).evalModules {
+  nixos = nixosSystem {
     modules = [
       {nixpkgs.hostPlatform = { system = "aarch64-linux"; };}
     ];
   };
 
-  result = toJSONLossy nixos.config;
-in assert result; null
+  final = toJSONLossy nixos.config;
+in assert final; null
