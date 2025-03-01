b2546adde1b403d966fbb8aa2958b94b801717d7
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-03-01 15:38:58 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:lib/tests/modules/serializeConfig.nix

--- lib/tests/modules/serializeConfig.nix
+++ lib/tests/modules/serializeConfig.nix
@@ -1,55 +1,13 @@
-{ lib, options, ... }:
-let
-in
-# unsafeGetAttrPos is unspecified best-effort behavior, so we only want to consider this test on an evaluator that satisfies some basic assumptions about this function.
-assert
-  builtins.unsafeGetAttrPos "a" (discardPositions {
-    a = true;
-  }) == null;
-{
-  imports = [
-    {
-      options.imported.line14 = lib.mkOption {
-        type = lib.types.int;
-      };
-
-      # Simulates various patterns of generating modules such as
-      # programs.firefox.nativeMessagingHosts.ff2mpv. We don't expect to get
-      # line numbers for these, but we can fall back on knowing the file.
-      options.generated = discardPositions {
-        line22 = lib.mkOption {
-          type = lib.types.int;
-        };
-      };
-
-      options.submoduleLine38.extraOptLine27 = lib.mkOption {
-        default = 1;
-        type = lib.types.int;
-      };
-    }
-  ];
-
-  options.nested.nestedLine34 = lib.mkOption {
-    type = lib.types.int;
+{ lib, ... }: {
+  options.result = lib.mkOption {
+    type = lib.types.str;
   };
+  config.result =
+    let
+      nixos = lib.nixosSystem {
+        modules = [ { nixpkgs.hostPlatform = "aarch64-linux"; } ];
+      };
 
-  options.submoduleLine38 = lib.mkOption {
-    default = { };
-    type = lib.types.submoduleWith {
-      modules = [
-        (
-          { options, ... }:
-          {
-            options.submodDeclLine45 = lib.mkOption { };
-          }
-        )
-        { freeformType = with lib.types; lazyAttrsOf (uniq unspecified); }
-      ];
-    };
-  };
-
-  config = {
-    result =
-  }
+      serialized = nixos.config.nixpkgs.hostPlatform.system;
+    in serialized;
 }
-
