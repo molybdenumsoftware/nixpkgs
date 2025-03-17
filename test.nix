68409fa556c4e2917d1a9ed571e82b991d54ea11
Alexander Foremny <aforemny@posteo.de>
2025-03-17 15:46:23 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:test.nix

--- test.nix
+++ test.nix
@@ -23,16 +23,22 @@ let
   # stack overflow (arbitrary depth limit) (possibly configurable?)
 
   toJSONLossy =
-      path: maybe:
-      let
-        result = builtins.catchEvalErrors maybe;
-        val = if result.success then result.value else "«error»";
-      in
-      lib.trace path
+    path: maybe:
+    let
+      result = builtins.catchEvalErrors maybe;
+      val = if result.success then result.value else "«error»";
+    in
+    if builtins.length path > 10 then "«infrec»" else
+    (lib.trace (lib.concatStrings path)
       (
-        #if (lib.length path) else
-        if lib.isDerivation val then
-          "«derivation»"
+        if lib.isFunction val then
+          "«function»"
+        else if lib.isAttrs val then
+          let
+            hasType = val ? type && (builtins.catchEvalErrors val.type).success;
+          in
+          if hasType && lib.isDerivation val then
+            "«derivation»"
           # let
           #   result = builtins.tryEval val.drvPath;
           # in
@@ -40,28 +46,26 @@ let
           #     "«derivation ${val.drvPath}»"
           #   else
           #     "«error: failed to evaluate derivation»"
-        else if lib.isFunction val then
-          "«function»"
-        else if lib.isAttrs val then
-          if val ? drvPath then
+          else if hasType && val ? drvPath then
             "«what is this undocumented derivationStrict?»"
           else
             lib.pipe val [
-              (lib.flip lib.removeAttrs ["assertions"])
-              (lib.mapAttrs (name: value: toJSONLossy "${path}.${name}" value))
+              (lib.flip lib.removeAttrs [ "assertions" ])
+              (lib.mapAttrs (name: value: toJSONLossy (path ++ [ ".${name}" ]) value))
               builtins.toJSON
             ]
         else if lib.isList val then
-          lib.imap0 (i: v: toJSONLossy "${path}[${toString i}]" v) val
+          lib.imap0 (i: v: toJSONLossy (path ++ [ "[${toString i}]" ]) v) val
         else
           builtins.toJSON val
-      );
+      ));
 
   nixos = nixosSystem {
     modules = [
-      {nixpkgs.hostPlatform = { system = "aarch64-linux"; };}
+      { nixpkgs.hostPlatform = { system = "aarch64-linux"; }; }
     ];
   };
 
-  final = toJSONLossy "" nixos.config ;
-in assert final; null
+  final = toJSONLossy [ ] nixos.config;
+in
+assert final; null
