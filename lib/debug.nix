71ec86c1cc197b6d1417bb2fb30d990fe89ca6b8
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-03-19 00:18:36 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:lib/debug.nix

--- lib/debug.nix
+++ lib/debug.nix
@@ -478,5 +478,60 @@ rec {
     expected = map (x: true) expr;
   };
 
+  catchAndDisplayRecursive =
+    let
+    omit =
+      path: x:
+        if
+          lib.any lib.id [
+            (
+              # TODO
+              lib.elem path [
+                [
+                  "virtualisation"
+                  "vmVariant"
+                ]
+                [
+                  "virtualisation"
+                  "vmVariantWithBootLoader"
+                ]
+              ]
+            )
+            (x._type or null == "pkgs")
+            (x ? recurseForDerivations)
+          ]
+        then
+          "«omitted»"
+        else
+          x;
 
+      display =
+        val:
+        if lib.isPath val then
+          "«path ${toString val}»"
+        else if lib.isFunction val then
+          "«function»"
+        else if lib.isDerivation val then
+          let
+            #result = builtins.catchEvalErrors val.drvPath; TODO
+            result = {
+              success = false;
+            };
+          in
+          "«derivation ${if result.success then result.value else "evaluation error"}»"
+        else
+          val;
+    in
+    x:
+    lib.pipe x [
+      (lib.trivial.mapRecursiveTopDown (
+        _: x:
+        let
+          result = builtins.catchEvalErrors x;
+        in
+        if result.success then result.value else "«evaluation error»"
+      ))
+      (lib.trivial.mapRecursiveTopDown omit)
+      (lib.trivial.mapRecursiveTopDown (_: display))
+    ];
 }
